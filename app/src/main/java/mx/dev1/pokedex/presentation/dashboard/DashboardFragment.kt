1b7e1bd3125e1c3e8fd74ab75047e385ce54139d
Marco Ramirez <marco.ramirez@rzerocorp.com>
2020-07-02 23:41:35 -0500

    Established Default colors for UX/UI

--- app/src/main/java/mx/dev1/pokedex/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/mx/dev1/pokedex/presentation/dashboard/DashboardFragment.kt
@@ -6,6 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.Nullable
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import mx.dev1.pokedex.R
 import mx.dev1.pokedex.presentation.ApiDependencies
 import org.koin.android.ext.android.inject
@@ -13,21 +19,69 @@ import org.koin.android.ext.android.inject
 class DashboardFragment : Fragment() {
 
     private lateinit var viewModel: DashboardViewModel
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
     private val dependencies: ApiDependencies by inject()
+
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Do Nothing
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+        var view = inflater.inflate(R.layout.dashboard_fragment, container, false)
+
+        var activity = requireActivity() as AppCompatActivity
+        toolbar = view.findViewById(R.id.tb_dashboard)
+        activity.setSupportActionBar(toolbar)
+
+        activity.supportActionBar!!.title = "Dashboard"
+        activity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         viewModel.dependencies = this.dependencies
+
+        initDrawerLayout(savedInstanceState)
+
         viewModel.getRegions()
         viewModel.getDetailedRegion("alola")
+    }
+
+    private fun initDrawerLayout(savedInstanceState: Bundle?) {
+        drawerLayout = requireView().findViewById(R.id.dl_dashboard)
+        drawerToggle = setupDrawerToggle()
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
+        drawerLayout.addDrawerListener(drawerToggle)
+    }
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        return ActionBarDrawerToggle(
+            requireActivity(),
+            drawerLayout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
     }
 
 }
