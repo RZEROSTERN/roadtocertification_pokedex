a7ed393ada4cd5ec560c516e84b832e8bbf211d1
Marco Ramirez <marco.ramirez@rzerocorp.com>
2020-07-09 01:52:07 -0500

    Added Layout for Pokemon

--- app/src/main/java/mx/dev1/pokedex/presentation/pokedexinfo/adapters/PokemonsAdapter.kt
+++ app/src/main/java/mx/dev1/pokedex/presentation/pokedexinfo/adapters/PokemonsAdapter.kt
@@ -1,14 +1,17 @@
 package mx.dev1.pokedex.presentation.pokedexinfo.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import mx.dev1.pokedex.R
 import mx.dev1.pokedex.core.domain.Pokemon
 
 class PokemonsAdapter(private val items: MutableList<Pokemon>): RecyclerView.Adapter<PokemonsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
+        var layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.pokemon_button_item, parent, false))
     }
 
     override fun getItemCount(): Int {
